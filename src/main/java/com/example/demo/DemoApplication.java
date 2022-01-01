8e8078e49cdcae977fb15b4bba16c29d35a9d314
Stefanija Ristomanova <stefanija.ristomanova@yahoo.com>
2022-01-01 03:32:23 +0100

    Add student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +19,8 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello(){
-		return "Hello world";
+	public List<Student> hello(){
+		return List.of(new Student(1L, "Mariam", "mariam.jamal@gmail.com",  LocalDate.of(2000, Month.JANUARY, 5), 21));
 	}
 
 }
